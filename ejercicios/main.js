afea4d08942096f1653aa16584dae738470f567e
josebarriosm <josebarriosm@hotmail.com>
2024-10-16 08:15:37 -0500

    Ordenamiento en burbuja

--- ejercicios/main.js
+++ ejercicios/main.js
@@ -1,5 +1,6 @@
 console.log(
-  "FUNCION PUSH Y FOREACH ------------------------------------------ "
+  "%cFUNCION PUSH Y FOREACH ------------------------------------------ ",
+  "background-color:blue;color:#fff;padding: 5px 10px;"
 );
 let listaMercado = ["Arroz", "Aceite", "Pan", "Harina"];
 console.log("Se pinta el array -> " + listaMercado);
@@ -15,7 +16,10 @@ listaMercado.forEach((item, index) => {
 const array1 = [1, 4, 9, 16];
 // Pass a function to map
 const map1 = array1.map((x) => x + 1);
-console.log("\nFUNCION MAP ------------------------------------------ ");
+console.log(
+  "\n%cFUNCION MAP ------------------------------------------ ",
+  "background-color:blue;color:#fff;padding: 5px 10px;"
+);
 console.log("Array original de números -> " + array1);
 console.log(
   "Nuevo array modificado sumando un 1 a cada elemento con map -> " + map1
@@ -35,21 +39,55 @@ console.log(
 );
 
 /* FIZZ ----------------------------------------------------------*/
-console.log("\nFIZZBUZZ ------------------------------------------ ");
-for (let i = 1; i <= 100; i++) {
+console.log(
+  "\n%cFIZZBUZZ ------------------------------------------ ",
+  "background-color:blue;color:#fff;padding: 5px 10px;"
+);
+console.log(
+  "Múltipos de 3 (fizz) - múltiplos de 5 (buzz) y múltiplos de ambos (fizzbuzz)"
+);
+for (let i = 1; i <= 50; i++) {
   let numero = i;
   multiploDe3 = numero % 3;
   multiploDe5 = numero % 5;
 
   if (multiploDe3 == 0 && multiploDe5 == 0) {
-    console.log("fizzbuzz");
+    console.log(
+      "%c" + i + " - FIZZBUZZ => múltiplo de 3 y 5",
+      "color:green;padding: 5px 10px;font-weight:800"
+    );
   } else if (multiploDe3 == 0) {
     //console.log(`El número ${numero} es múltiplo de 3`);
-    console.log("fizz");
+    console.log(
+      "%c" + i + " - FIZZ => múltiplo de 3",
+      "color:orange;padding: 5px 10px;font-weight:800"
+    );
   } else if (multiploDe5 == 0) {
     //console.log(`El número ${numero} es múltiplo de 5`);
-    console.log("buzz");
+    console.log(
+      "%c" + i + " - BUZZ => múltiplo de 5",
+      "color:blue;padding: 5px 10px;font-weight:800"
+    );
   } else {
     console.log(numero);
   }
 }
+
+console.log(
+  "\n%cORDENAMIENTO EN BURBUJA  ------------------------------------------ ",
+  "background-color:blue;color:#fff;padding: 5px 10px;"
+);
+let arreglo = [35, 5, 50, 1, 120, 7, 1, 3, 85, 65];
+console.log("Arreglo original -> " + arreglo);
+function ordenarBurbuja(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      let aux = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = aux;
+      ordenarBurbuja(array);
+    }
+  }
+  return array;
+}
+console.log("Arreglo ordenado -> " + ordenarBurbuja(arreglo));
